ff433b30489755ca8389c99936ef90730074e76c
GuillaumePons63 <guillaume@web-63.fr>
2024-03-31 13:19:52 +0200

    hello world

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import 'dotenv/config'
 
 import express from 'express';
+import router from './routes/index.js';
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+
+app.use(router);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
