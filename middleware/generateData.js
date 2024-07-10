339223a1995289a5e64f61e9fc58a1c93ca7f226
GuillaumePons63 <guillaume@web-63.fr>
2024-07-10 10:51:25 +0200

    add skeleton css

--- middleware/generateData.js
+++ middleware/generateData.js
@@ -4,11 +4,7 @@ import { fakerFR as faker } from '@faker-js/faker'
 const generateData = (req, res, next) => {
     const data = [];
     for (let i = 0; i < req.body.rows; i++) {
-        const firstName = faker.person.firstName();
-        const lastName = faker.person.lastName();
-        const email = faker.internet.email({ firstName, lastName });
-        const phone = faker.phone.number();
-        const country = faker.location.country();
+        const { firstName, lastName, email, phone, country } = oneData();
         data.push({ firstName, lastName, email, phone, country });
     }
     data.columns = ['First Name', 'Last Name', 'Email', 'Phone', 'Country'];
@@ -16,4 +12,16 @@ const generateData = (req, res, next) => {
     next();
 }
 
+
+const oneData = () => {
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    const email = faker.internet.email({ firtName: firstName, lastName: lastName }).toLowerCase();
+    const phone = faker.phone.number();
+    const country = faker.location.country();
+    return { firstName, lastName, email, phone, country };
+}
+
+
 export default generateData;
+
